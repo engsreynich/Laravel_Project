6530c939460586aeb2151b1f36d8a0607bdc5fae
engsreynich <esreynich6@gmail.com>
2025-05-04 16:53:33 +0700

    -How to route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+#Create_edit
+Route::get('/create_edit', function () {
+    return view('category.create_edit');
+})->name('create_edit');
+#category_category
+Route::get('/category', function () {
+    return view('category.category');
+})->name('categoty');
+
+#Blog
+Route::get('/blog', function () {
+    return view('Blog');
+})->name('blog');
+
+#login
+Route::get('/login', function () {
+    return view('Login');
+});
+
+#MVC - Model View Controller
+
+
